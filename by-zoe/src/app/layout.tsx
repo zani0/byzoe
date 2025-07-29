b88981d58fbd3891afcef28d0701c29a4c875271
Zoe <zoe@berthengineering.com>
2025-07-29 00:14:46 +0000

    updated fonts

--- by-zoe/src/app/layout.tsx
+++ by-zoe/src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const bricolageGrotesque = Bricolage_Grotesque({
+  variable: "--font-bricolage-grotesque",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: "400"
 });
 
 export const metadata: Metadata = {
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${bricolageGrotesque.variable} antialiased`}
       >
         {children}
       </body>
--- by-zoe/src/app/page.tsx
+++ by-zoe/src/app/page.tsx
@@ -12,13 +12,13 @@ export default function Navigation() {
     <header className="bg-black">
       <nav className="mx-auto flex max-w-7xl items-center justify-center p-6 lg:px-6">
         <div className="flex lg:flex-1">
-          <Link href="">by zoe</Link>
+          <Link className="text-white" href="">by zoe</Link>
         </div>
         <div className="">
           {links.map((item) => (
             <Link
               href={item.url}
-              className="bg-pink-300 px-6 py-2 rounded-full text-black mr-2 text-[12px] lowercase"
+              className="bg-pink-300 hover:bg-white active:bg-gray-50 px-6 py-2 rounded-full text-black mr-2 text-[12px] lowercase"
             >
               {item.label}
             </Link>
