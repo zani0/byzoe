860debb04c5786705a111e3127d9c2c92fa9429c
Zoe <zoe@berthengineering.com>
2025-07-29 00:21:54 +0000

    added hero

--- by-zoe/src/app/page.tsx
+++ by-zoe/src/app/page.tsx
@@ -8,23 +8,34 @@ export default function Navigation() {
     { label: "Contact", url: "#contact" },
   ];
   return (
-    // Navigation
-    <header className="bg-black">
-      <nav className="mx-auto flex items-center justify-center p-6 lg:px-12">
-        <div className="flex lg:flex-1">
-          <Link className="text-white text-[20px]" href="">by zoe.</Link>
-        </div>
-        <div className="">
-          {links.map((item) => (
-            <Link
-              href={item.url}
-              className="bg-pink-300 hover:bg-white active:bg-gray-50 px-6 py-2 rounded-full text-black mr-2 text-[14px] lowercase"
-            >
-              / {item.label}
+    <>
+      {/* Navigation */}
+      <header className="bg-black">
+        <nav className="mx-auto flex items-center justify-center p-6 lg:px-12">
+          <div className="flex lg:flex-1">
+            <Link className="text-white text-[20px]" href="">
+              by zoe.
             </Link>
-          ))}
+          </div>
+          <div className="">
+            {links.map((item) => (
+              <Link
+                href={item.url}
+                className="bg-pink-300 hover:bg-white active:bg-gray-50 px-6 py-2 rounded-full text-black mr-2 text-[14px] lowercase"
+              >
+                / {item.label}
+              </Link>
+            ))}
+          </div>
+        </nav>
+      </header>
+      {/* Hero */}
+      <section className="w-full h-screen justify-center items-center flex">
+        <div className="text-center">
+          <h1>everything </h1>
+          <h1>by zoe</h1>
         </div>
-      </nav>
-    </header>
+      </section>
+    </>
   );
 }
