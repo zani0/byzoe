a5bc45a1f27b0d8569c93e1491557dd70296a45d
Zoe <zoe@berthengineering.com>
2025-07-29 12:17:55 +0000

    updated list keys

--- by-zoe/src/app/layout.tsx
+++ by-zoe/src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${bricolageGrotesque.variable} antialiased`}
+        className={`${bricolageGrotesque.variable} antialiased lowercase`}
       >
         {children}
       </body>
--- by-zoe/src/app/page.tsx
+++ by-zoe/src/app/page.tsx
@@ -11,22 +11,22 @@ export default function Navigation() {
   const projects = [
     {
       name: "lorem",
-      image: "/project.jpg",
+      image: "/project-2.jpg",
       url: "#",
       description:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati, inventore dignissimos!",
       category: "web development",
     },
     {
-      name: "lorem",
-      image: "/project.jpg",
+      name: "lorem ipsum",
+      image: "/project-1.jpg",
       url: "#",
       description:
         "Fugit dignissimos excepturi recusandae voluptatibus, minima quidem rerum modi.",
       category: "web development",
     },
     {
-      name: "lorem",
+      name: "lorem ipsum dolor",
       image: "/project.jpg",
       url: "#",
       description:
@@ -48,6 +48,7 @@ export default function Navigation() {
           <div className="">
             {links.map((item) => (
               <Link
+                key={item.label}
                 href={item.url}
                 className="bg-pink-300 hover:bg-white active:bg-gray-50 px-6 py-2 rounded-full text-black mr-2 text-[14px] lowercase"
               >
@@ -114,7 +115,7 @@ export default function Navigation() {
         </h1>
         <div className="grid lg:grid-cols-3 gap-4">
           {projects.map((item) => (
-            <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+            <figure key={item.name} className="relative max-w-full min-h-[400px] transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
               <Link href="#">
                 <Image
                   className="rounded-lg"
@@ -125,13 +126,13 @@ export default function Navigation() {
                 />
               </Link>
               <figcaption className="absolute px-4 lowercase text-white bottom-6">
-                <h1 className="lg:text-[25px]">{item.name}</h1>
-                <p className="lg:text-[14px] mb-4">
+                <h1 className="lg:text-[25px] text-[18px]">{item.name}</h1>
+                <p className="lg:text-[14px] text-[14px] mb-4">
                   {item.description}
                 </p>
                 <Link
                   href={item.url}
-                  className="bg-white text-black hover:bg-white/90 px-6 py-3 rounded-full block w-full text-center"
+                  className="bg-white text-black hover:bg-pink-300/70 transition-all px-6 py-3 rounded-full block w-full text-center"
                 >
                   See more
                 </Link>
@@ -139,6 +140,12 @@ export default function Navigation() {
             </figure>
           ))}
         </div>
+      </section>
+      {/* contact */}
+      <section id="contact" className="">
+        <h1 className="lg:text-[35px] text-[20px] text-center text-pink-300 mb-6">
+          contact/
+        </h1>
       </section>
     </>
   );
